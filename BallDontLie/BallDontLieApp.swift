b46e33a36d46e98d0054bce26936cf29d9bbd4b6
Mehmet Tarhan <hi@memtarhan.com>
2024-12-13 12:16:01 +0300

    Create iOS app

---
+++ BallDontLie/BallDontLieApp.swift
@@ -0,0 +1,17 @@
+//
+//  BallDontLieApp.swift
+//  BallDontLie
+//
+//  Created by Mehmet Tarhan on 13.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct BallDontLieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BallDontLie/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BallDontLie
+//
+//  Created by Mehmet Tarhan on 13.12.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
